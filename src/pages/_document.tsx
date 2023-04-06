b1bd08d048c2817b47c6f8dcad450f6c4c90877b
YactayoC <syactayocrisostomo@gmail.com>
2023-04-06 15:11:26 -0500

    add metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/png" href="/assets/logo.png" />
+        <meta name="description" content="Sebastian Yactayo Developer - Portfolio" />
+        <meta name="keywords" content="YactayoC, Sebastian Yactayo, Portfolio" />
+        <meta name="author" content="Sebastian Yactayo" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body>
         <Main />
